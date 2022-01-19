7f5d85a17920a675840b3fa56e7bbabaeba9e5fa
Ntanis <me@ntanis.dev>
2022-01-19 22:04:31 +0200

    typo

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
     // Enter here the Discord webhook of the channel you want to post to.
     discordWebhookUrl: '',
 
-    // Which identifier to post on Discord? If your server does not require Steam using something like 'license'.
+    // Which identifier to post on Discord? If your server does not require Steam, use something like 'license'.
     logIdentifier: 'steam',
 
     // Do not change unless you have renamed cs-stories to something else.
